e68e5da6f9229f1f61dfb073b5b3a601441350c8
Jennifer Parker <jennifer.parker1@verizon.com>
2019-07-31 15:02:35 -0500

    afternoon commit

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 const data = require('./data.json');
-const { projects } = data;
+const projects = data.projects
 
 
 app.set("view engine", "pug");
@@ -10,28 +10,36 @@ app.use('/static', express.static('public'));
 app.use('/static', express.static('images'));
 
 
-// routing to index page
+// routing to home page
 app.get('/', (req, res, next) => {
   res.render('index', { projects: projects });
 });
 
-//route to about pagenode 
+//route to about page
 app.get('/about', (req, res, next) => {
   res.render('about');
 });
 
 // creating route to projects page
 
-app.get('/project_:i([0-4])', (req, res, next) => {
-  const { i } = req.params;
-  const { image_urls } = projects[i];
-  const { project_name } = projects[i];
-  const { description } = projects[i];
-  const { technologies } = projects[i];
-  const { live_link } = projects[i];
-  const { github_link } = projects[i];
-  res.render('project', {project_name, description, technologies, live_link, github_link, image_urls});
-});
+app.get('/project/:id', (req, res) => res.render('project', {
+  title: projects[req.params.id].project_name,
+  description: projects[req.params.id].description,
+  technologies: projects[req.params.id].technologies,
+  liveLink: projects[req.params.id].live_link,
+  githubLink: projects[req.params.id].github_link,
+  image: projects[req.params.id].image_urls
+}));
+// app.get('/project_:i([0-4])', (req, res, next) => {
+//   const { i } = req.params;
+//   const { image_urls } = projects[i];
+//   const { project_name } = projects[i];
+//   const { description } = projects[i];
+//   const { technologies } = projects[i];
+//   const { live_link } = projects[i];
+//   const { github_link } = projects[i];
+//   res.render('project', {project_name, description, technologies, live_link, github_link, image_urls});
+// });
 
 // catching 404 errors
 app.use((req, res, next) => {
