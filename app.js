b9930d653d0f43b5963cb53a8408573abc26185c
Jennifer Parker <jennifer.parker1@verizon.com>
2019-08-02 12:21:25 -0500

    prettified code

--- app.js
+++ app.js
@@ -1,39 +1,34 @@
-const express = require('express');
+const express = require("express");
 const app = express();
 
-const { projects }  = require('./data.json');
- 
-
+const { projects } = require("./data.json");
 
 app.set("view engine", "pug");
-app.use('/static', express.static('public'));
-app.use('/static', express.static('images'));
-
+app.use("/static", express.static("public"));
+app.use("/static", express.static("images"));
 
 // routing to home page
-app.get('/', (req, res, next) => {
-  res.render('index', { projects: projects });
+app.get("/", (req, res, next) => {
+  res.render("index", { projects: projects });
 });
 
 //route to about page
-app.get('/about', (req, res, next) => {
-  res.render('about');
+app.get("/about", (req, res, next) => {
+  res.render("about");
 });
 
 // creating route to projects page
 
-app.get('/project/:id', (req, res) => {
-res.render('project',{
-  title: projects[req.params.id].project_name,
-  description: projects[req.params.id].description,
-  technologies: projects[req.params.id].technologies,
-  liveLink: projects[req.params.id].live_link,
-  githubLink: projects[req.params.id].github_link,
-  image: projects[req.params.id].image_urls
-})});
-
-
-
+app.get("/project/:id", (req, res) => {
+  res.render("project", {
+    title: projects[req.params.id].project_name,
+    description: projects[req.params.id].description,
+    technologies: projects[req.params.id].technologies,
+    liveLink: projects[req.params.id].live_link,
+    githubLink: projects[req.params.id].github_link,
+    image: projects[req.params.id].image_urls
+  });
+});
 
 //catching 404 errors
 app.use((req, res, next) => {
@@ -43,16 +38,14 @@ app.use((req, res, next) => {
   next(err);
 });
 
-
-
 app.use((err, req, res, next) => {
   res.locals.error = err;
   res.status(err.status);
   console.log(err);
-  res.render('error');
+  res.render("error");
 });
 // and finally listen on port 3000
 app.listen(3000, () => {
   // and print a message if everything is successful
-  console.log('Up and running at localhost, port 3000!');
+  console.log("Up and running at localhost, port 3000!");
 });
